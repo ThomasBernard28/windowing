71513d30ef5d90fcfb85326f9314f30f47290831
Theo <210170@umons.ac.be>
2023-01-14 22:15:52 +0100

    small changes

--- app/src/main/java/windowing/scenes/SegmentsScene.java
+++ app/src/main/java/windowing/scenes/SegmentsScene.java
@@ -2,6 +2,7 @@ package windowing.scenes;
 
 import windowing.datastructures.Segment;
 import javafx.scene.Scene;
+import javafx.scene.Group;
 import javafx.scene.layout.StackPane;
 import javafx.scene.control.*;
 import javafx.scene.shape.Line;
@@ -14,15 +15,19 @@ public class SegmentsScene extends Scene {
     public SegmentsScene(StackPane root) {
         super(root, 1000, 500);
         this.root = root;
-        Button button = new Button();
     }
     
+    /**
+     * @Param segments : Segments to be displayed
+    **/
     public void show_segments(ArrayList<Segment> segments) {
         ArrayList<Double> c; 
+        Group group = new Group();
         for ( Segment s : segments ) {
             c = s.get_coords();
             Line l = new Line(c.get(0), c.get(1), c.get(2), c.get(3)); 
-            root.getChildren().add(l);
+            group.getChildren().add(l);
         }
+        root.getChildren().add(group);
     }
 }
