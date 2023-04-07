033485a58bbb891ae8dbbaef775db63d11a069c2
Thomas <thomas.bernard@student.umons.ac.be>
2023-04-07 15:54:30 +0200

    implemented query_pst() and report_in_subtree() not working yet

--- app/src/main/java/windowing/datastructures/PrioritySearchTree.java
+++ app/src/main/java/windowing/datastructures/PrioritySearchTree.java
@@ -2,6 +2,7 @@ package windowing.datastructures;
 
 import org.checkerframework.checker.units.qual.C;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Collections;
 
@@ -18,13 +19,13 @@ public class PrioritySearchTree {
         this.segments = segments;
         nodes = new ArrayList<Node>();
         // we start by creating our set of points i.e. all segments endpoints 
-        for ( Segment s : segments ) {
+        for (Segment s : segments) {
             // We only need to take the starting component for the tree
             nodes.add(new Node(s.get_startComp(), s));
             //nodes.add(new Node(s.get_endComp(), s));
         }
         // points are sorted to improve the efficiency when calculating the median
-        quicksort(nodes, 0, nodes.size()-1);
+        quicksort(nodes, 0, nodes.size() - 1);
         // DEBUG nodes.forEach((n) -> System.out.println(n));
         // now we can construct the tree 
         construct_tree(nodes);
@@ -50,9 +51,10 @@ public class PrioritySearchTree {
     }
 
     /**
+     *
      */
-    private Node construct_tree(ArrayList<Node> nodes){
-        if ( nodes.size() > 1 ) {
+    private Node construct_tree(ArrayList<Node> nodes) {
+        if (nodes.size() > 1) {
 
             // finding the root of the tree
             data = nodes.get(find_min_index(nodes));
@@ -65,14 +67,13 @@ public class PrioritySearchTree {
             ArrayList<Node> leftTree = new ArrayList<Node>();
             ArrayList<Node> rightTree = new ArrayList<Node>();
 
-            for (Node node : nodes){
-                if (node.point.is_y_smaller_than(compositeMedian)|| node.point.is_equal_to(compositeMedian)){
+            for (Node node : nodes) {
+                if (node.point.is_y_smaller_than(compositeMedian) || node.point.is_equal_to(compositeMedian)) {
                     //System.out.println("-------- I'm adding data in left tree --------");
                     //System.out.println("Point : " + node.point.toString());
                     //System.out.println("Median : " + compositeMedian.toString());
                     leftTree.add(node);
-                }
-                else{
+                } else {
                     //System.out.println("-------- I'm adding nodes in right tree ---------");
                     //System.out.println("Point : " + node.point.toString());
                     //System.out.println("Median : " + compositeMedian.toString());
@@ -88,18 +89,18 @@ public class PrioritySearchTree {
             return data;
         }
 
-        if ( nodes.size() == 1 ) {
-            data = nodes.get(0); 
+        if (nodes.size() == 1) {
+            data = nodes.get(0);
             nodes.clear();
         }
         return null;
     }
 
 
-    private int find_min_index(ArrayList<Node> nodes){
+    private int find_min_index(ArrayList<Node> nodes) {
         int index = 0;
-        for ( int n=1; n<nodes.size(); n++ ) {
-            if ( nodes.get(n).point.is_x_smaller_than(nodes.get(index).point) ) {
+        for (int n = 1; n < nodes.size(); n++) {
+            if (nodes.get(n).point.is_x_smaller_than(nodes.get(index).point)) {
                 index = n;
             }
         }
@@ -107,57 +108,57 @@ public class PrioritySearchTree {
     }
 
     private void quicksort(ArrayList<Node> nodes, int start, int end) {
-        if ( start < end ) {
+        if (start < end) {
             int part = partition(nodes, start, end);
-            quicksort(nodes, start, part-1);
-            quicksort(nodes, part+1, end);
+            quicksort(nodes, start, part - 1);
+            quicksort(nodes, part + 1, end);
         }
     }
 
     private int partition(ArrayList<Node> nodes, int start, int end) {
         Node node = nodes.get(end);
         int i = start - 1;
-        for ( int j=start; j<=end-1; j++ ) {
-            if ( nodes.get(j).point.is_y_smaller_than(node.point) ) {
+        for (int j = start; j <= end - 1; j++) {
+            if (nodes.get(j).point.is_y_smaller_than(node.point)) {
                 i++;
                 Collections.swap(nodes, i, j);
             }
         }
-        Collections.swap(nodes, i+1, end);
-        return i+1;
+        Collections.swap(nodes, i + 1, end);
+        return i + 1;
     }
 
-    private double find_median_value(ArrayList<Node> nodes){
-        if ( nodes.size() == 1 ) {
+    private double find_median_value(ArrayList<Node> nodes) {
+        if (nodes.size() == 1) {
             return nodes.get(0).point.get_coord2();
         }
-        if ( nodes.size() % 2 == 0 ) {
-            return nodes.get(nodes.size()/2).point.get_coord2();
+        if (nodes.size() % 2 == 0) {
+            return nodes.get(nodes.size() / 2).point.get_coord2();
         }
-        return nodes.get(nodes.size()/2 +1).point.get_coord2();
+        return nodes.get(nodes.size() / 2 + 1).point.get_coord2();
     }
 
     // Method to compute a CompositeNumber median in order to sort properly all points
     // considering the case where all y coordinates in the set are equals.
-    private CompositeNumber find_median(ArrayList<Node> nodes){
+    private CompositeNumber find_median(ArrayList<Node> nodes) {
         // Median will be a composite number (x|y) with the median on y and x on values.
         CompositeNumber median;
-        if (nodes.size() == 1){
-            median = new CompositeNumber(nodes.get(0).point.get_coord1(), nodes.get(0).point.get_coord2()/2);
+        if (nodes.size() == 1) {
+            median = new CompositeNumber(nodes.get(0).point.get_coord1(), nodes.get(0).point.get_coord2() / 2);
         }
-        if (nodes.size() % 2 == 0){
-            median = new CompositeNumber(((nodes.get((nodes.size()/2)-1).point.get_coord1())+(nodes.get(nodes.size()/2).point.get_coord1()))/2
-                    ,((nodes.get((nodes.size()/2)-1).point.get_coord2())+(nodes.get(nodes.size()/2).point.get_coord2()))/2);
-        }
-        else{
-            median = new CompositeNumber(nodes.get((nodes.size() - 1)/2).point.get_coord1(), nodes.get((nodes.size()-1)/2).point.get_coord2());
+        if (nodes.size() % 2 == 0) {
+            median = new CompositeNumber(((nodes.get((nodes.size() / 2) - 1).point.get_coord1()) + (nodes.get(nodes.size() / 2).point.get_coord1())) / 2
+                    , ((nodes.get((nodes.size() / 2) - 1).point.get_coord2()) + (nodes.get(nodes.size() / 2).point.get_coord2())) / 2);
+        } else {
+            median = new CompositeNumber(nodes.get((nodes.size() - 1) / 2).point.get_coord1(), nodes.get((nodes.size() - 1) / 2).point.get_coord2());
         }
         return median;
     }
 
     public ArrayList<Segment> query(double xMin, double yMin, double xMax, double yMax) {
         reportedSegments = new ArrayList<Segment>();
-        apply_window(xMin, yMin, xMax, yMax, reportedSegments);
+        //apply_window(xMin, yMin, xMax, yMax, reportedSegments);
+        query_pst(xMin, yMin, xMax, yMax, reportedSegments, false, false);
         return reportedSegments;
     }
 
@@ -165,23 +166,149 @@ public class PrioritySearchTree {
      * This is dogshit, it must be completely reworked.
      */
     private void apply_window(double xMin, double yMin, double xMax, double yMax, ArrayList<Segment> reportedSegments) {
-        if ( data != null ) {
-            double x = data.point.get_coord1(); 
-            double y = data.point.get_coord2(); 
+        if (data != null) {
+            double x = data.point.get_coord1();
+            double y = data.point.get_coord2();
             // first case : at least one endpoint lie within the window
-            if ( x>=xMin && x<=xMax && y>=yMin && y<=yMax ) {
-                if ( !reportedSegments.contains(data.segment) ) {
+            if (x >= xMin && x <= xMax && y >= yMin && y <= yMax) {
+                if (!reportedSegments.contains(data.segment)) {
                     reportedSegments.add(data.segment);
                 }
             }
-            if ( leftTree != null ) { 
+            if (leftTree != null) {
                 leftTree.apply_window(xMin, yMin, xMax, yMax, reportedSegments);
             }
-            if ( rightTree != null ) { 
+            if (rightTree != null) {
                 rightTree.apply_window(xMin, yMin, xMax, yMax, reportedSegments);
             }
-        // second case : the segment cross entierly the window i.e. no endpoint within the window
+            // second case : the segment cross entierly the window i.e. no endpoint within the window
         }
 
     }
+
+    private void query_pst(double xMin, double yMin, double xMax, double yMax, ArrayList<Segment> reportedSegments, boolean split, boolean maxSearching) {
+        //search unbounded to the left. vsplit is the node where two search path splits
+        // for each node on the search path of yMin yMax check if node is in range if so report it
+        // for each node on the path of yMin i.e in the left subtree of vsplit : Si le chemin continue à gauche du noeud v reportInSubtree( filsDroit(v), xMax)
+        // for each node on the path of yMax i.e int the right subtree of vsplit : Si le chemin continue à droite du noeud v reportInSubtree(filsGauche(v), xMax)
+        if (split){
+            if (data != null){
+                double x = data.point.get_coord1();
+                double y = data.point.get_coord2();
+                CompositeNumber median = data.compositeMedian;
+
+                // First look if the point needs to be reported.
+                if(x >= xMin && x <= xMax && y >= yMin && y <= yMax){
+                    if(!reportedSegments.contains(data.segment)){
+                        reportedSegments.add(data.segment);
+                    }
+                }
+                //Then we will consider if we are in the right or left subtree.
+                if(maxSearching){
+                    //We are in the right subtree of vsplit
+                    if (yMax > median.get_coord2()){
+                        // Then the leftsubtree will match the y condition and can be explored only on x condition
+                        rightTree.report_in_subtree(xMin, yMin, xMax, yMax, reportedSegments);
+                        //TODO implement report_in_subtree() method
+                    }
+                }
+                else{
+                    // We are in the left subtree of vsplit
+                    if (yMin <= median.get_coord2()){
+                        //Then the right subtree will match the y condition and can be explored only on x condition
+                        leftTree.report_in_subtree(xMin, yMin, xMax, yMax, reportedSegments);
+                        //TODO implement report_in_subtree() method
+                    }
+                }
+            }
+        }
+
+        else{
+            if(data != null){
+                double x = data.point.get_coord1();
+                double y = data.point.get_coord2();
+                CompositeNumber median = data.compositeMedian;
+
+                if (x >= xMin && x <= xMax && y>= yMin && y <= yMax){
+                    if(!reportedSegments.contains(data.segment)){
+                        reportedSegments.add(data.segment);
+                    }
+                    // In this cas we only need to go in the left subtree
+                    if (yMax <= median.get_coord2()){
+                        leftTree.query_pst(xMin, yMin, xMax, yMax, reportedSegments, false, false);
+                    }
+                    //In this case we only need to go in the right subtree and we didn't find the vsplit
+                    else if (yMin >= median.get_coord2()){
+                        rightTree.query_pst(xMin, yMin, xMax, yMax, reportedSegments, false, false);
+                    }
+                    else{
+                        //We've found the vsplit point and so we need to explore both subtree
+                        // In the case of the left subTree we are looking for the min condition
+                        if(leftTree != null){
+                            leftTree.query_pst(xMin, yMin, xMax, yMax, reportedSegments, true, false);
+                        }
+                        // In the cas of the right subTree we are looking for the max condition
+                        if (rightTree != null){
+                            rightTree.query_pst(xMin, yMin, xMax, yMax, reportedSegments, true, true);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public void report_in_subtree(double xMin, double yMin, double xMax, double yMax, ArrayList<Segment> reportedSegments){
+        if (data != null){
+            // Si notre point a une extrémité dans la fenêtre
+            if (data.point.get_coord1() <= xMax && data.point.get_coord1() >= xMin){
+                if(!reportedSegments.contains(data.segment)){
+                    reportedSegments.add(data.segment);
+                }
+                if(leftTree != null){
+                    leftTree.report_in_subtree(xMin, yMin, xMax, yMax, reportedSegments);
+                }
+                if(rightTree != null){
+                    rightTree.report_in_subtree(xMin, yMin, xMax, yMax, reportedSegments);
+                }
+            }
+            // Si notre point n'a pas l'extrémité considérée dans la fenêtre
+            else{
+                CompositeNumber endPoint = data.segment.get_endComp();
+                if (endPoint.get_coord1() <= xMax && endPoint.get_coord1()>= xMin){
+                    if(endPoint.get_coord2() >= yMin && endPoint.get_coord2() <= yMax){
+                        //Alors l'autre extrémité du segment est dans la fenêtre
+                        //On rapporte le segment
+                        if(!reportedSegments.contains(data.segment)){
+                            reportedSegments.add(data.segment);
+                        }
+                        if(leftTree != null){
+                            leftTree.report_in_subtree(xMin, yMin, xMax, yMax, reportedSegments);
+                        }
+                        if(rightTree != null){
+                            rightTree.report_in_subtree(xMin, yMin, xMax, yMax, reportedSegments);
+                        }
+                    }
+                }
+                else{
+                    //Dans ce cas aucune des deux extrémités ne figure dans le fenêtre. Il faut tout de même
+                    //Tester si le segment ne traverse pas la fenêtre.
+                    double middleX = (data.point.get_coord1() + endPoint.get_coord1())/2;
+                    double middleY = (data.point.get_coord2() + endPoint.get_coord2())/2;
+                    if (middleX >= xMin && middleX <= xMax && middleY >= yMin && middleY <= yMax){
+                        // Alors le segment traverse bien la fenêtre
+                        if (!reportedSegments.contains(data.segment)){
+                            reportedSegments.add(data.segment);
+                        }
+                        if(leftTree != null){
+                            leftTree.report_in_subtree(xMin, yMin, xMax, yMax, reportedSegments);
+                        }
+                        if(rightTree != null){
+                            rightTree.report_in_subtree(xMin, yMin, xMax, yMax, reportedSegments);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
 }
