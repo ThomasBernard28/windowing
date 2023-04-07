d1a826d816a4040e0445c8d38ae81483cdc47532
Thomas <thomas.bernard@student.umons.ac.be>
2023-04-07 18:00:43 +0200

    renamed Main to Test in order to match the indications

--- app/src/main/java/windowing/Test.java
+++ app/src/main/java/windowing/Test.java
@@ -1,14 +1,12 @@
 package windowing;
 
 import windowing.scenes.*;
-import windowing.AppWindowing;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.layout.VBox;
-import javafx.scene.Scene;
 
-public class Main extends Application {
+public class Test extends Application {
 
     public static void main(String[] args) {
         launch(args);
