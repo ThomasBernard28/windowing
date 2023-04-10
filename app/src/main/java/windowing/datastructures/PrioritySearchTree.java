1b7cc27fc405110323705f26efcb20583fb0d192
Thomas <thomas.bernard@student.umons.ac.be>
2023-04-10 17:07:34 +0200

    JavaDoc of the PrioritySearchTree

--- app/src/main/java/windowing/datastructures/PrioritySearchTree.java
+++ app/src/main/java/windowing/datastructures/PrioritySearchTree.java
@@ -3,6 +3,10 @@ package windowing.datastructures;
 import java.util.ArrayList;
 import java.util.Collections;
 
+/**
+ * This class contains all the methods related to the Priority Search Tree.
+ * From its construction to its querying.
+ */
 public class PrioritySearchTree {
 
     private Node data;
@@ -10,10 +14,12 @@ public class PrioritySearchTree {
     private PrioritySearchTree rightTree;
     private ArrayList<Segment> segments;
     private ArrayList<Node> nodes;
-    private ArrayList<Segment> reportedSegments;
 
-    private ArrayList<PrioritySearchTree> reportedSubTrees;
-
+    /**
+     * Constructor of the PST. During the construction we only keep the first endpoint of the segment.
+     * After creating nodes, we sort them with a quick sort based on their y component.
+     * @param segments all the segments that need to be inserted in the PST
+     */
     public PrioritySearchTree(ArrayList<Segment> segments) {
         this.segments = segments;
         nodes = new ArrayList<Node>();
@@ -48,7 +54,12 @@ public class PrioritySearchTree {
     }
 
     /**
-     *
+     * This method is used to construct the tree. It is a recursive method.
+     * First the method search the lowest point in the set of nodes based on its x coordinate. This point will be the root of the tree.
+     * Then the method computes the median of the set of nodes based on their y coordinate.
+     * Finally the method separates the set of nodes into two sets, one for the left tree and one for the right tree.
+     * @param nodes the set of nodes that need to be inserted in the tree
+     * @return the root of the tree
      */
     private Node construct_tree(ArrayList<Node> nodes) {
         if (nodes.size() > 1) {
@@ -83,14 +94,17 @@ public class PrioritySearchTree {
 
         if (nodes.size() == 1) {
             data = nodes.get(0);
-            CompositeNumber median = find_median(nodes);
-            data.compositeMedian = median;
+            data.compositeMedian = find_median(nodes);
             nodes.clear();
         }
         return null;
     }
 
-
+    /**
+     * This method is used to find the index of the lowest point in the set of nodes based on its x coordinate.
+     * @param nodes the set of nodes
+     * @return the index of the lowest point
+     */
     private int find_min_index(ArrayList<Node> nodes) {
         int index = 0;
         for (int n = 1; n < nodes.size(); n++) {
@@ -101,6 +115,12 @@ public class PrioritySearchTree {
         return index;
     }
 
+    /**
+     * This method implements the QuickSort from SDDII theory It sorts the set of nodes based on their y coordinate.
+     * @param nodes the set of nodes
+     * @param start the start index
+     * @param end  the end index
+     */
     private void quicksort(ArrayList<Node> nodes, int start, int end) {
         if (start < end) {
             int part = partition(nodes, start, end);
@@ -109,6 +129,14 @@ public class PrioritySearchTree {
         }
     }
 
+    /**
+     * This method is an annex to the Quicksort method. It is used to partition the set of nodes.
+     * The pivot is the point where all the nodes before the pivot are smaller than the pivot and all the nodes after the pivot are bigger than the pivot.
+     * @param nodes the set of nodes
+     * @param start the start index
+     * @param end   the end index
+     * @return the index of the pivot
+     */
     private int partition(ArrayList<Node> nodes, int start, int end) {
         Node node = nodes.get(end);
         int i = start - 1;
@@ -122,18 +150,17 @@ public class PrioritySearchTree {
         return i + 1;
     }
 
-    private double find_median_value(ArrayList<Node> nodes) {
-        if (nodes.size() == 1) {
-            return nodes.get(0).point.get_coord2();
-        }
-        if (nodes.size() % 2 == 0) {
-            return nodes.get(nodes.size() / 2).point.get_coord2();
-        }
-        return nodes.get(nodes.size() / 2 + 1).point.get_coord2();
-    }
 
     // Method to compute a CompositeNumber median in order to sort properly all points
     // considering the case where all y coordinates in the set are equals.
+
+    /**
+     * This method is used to compute the median of the set of nodes based on their y coordinate.
+     * It's a simple algorithm that uses the theory about the median of a sorted list.
+     * In this case the median is a Composite Number. This prevents the case where all y coordinates are equals during the PST construction.
+     * @param nodes the set of nodes
+     * @return A Composite Number (x|y) that is the median of the set of nodes
+     */
     private CompositeNumber find_median(ArrayList<Node> nodes) {
         // Median will be a composite number (x|y) with the median on y and x on values.
         CompositeNumber median;
@@ -149,9 +176,20 @@ public class PrioritySearchTree {
         return median;
     }
 
+    /**
+     * This method is used to instantiate the ArrayList of the reported segments.
+     * The reportedSubTrees ArrayList is used to store the subtrees that are reported by the query_pst() method.
+     * These subtrees will then be explored by the report_in_subtree() method in order to find the segments that need to be reported.
+     * @param xMin the minimum x coordinate of the window
+     * @param xMax the maximum x coordinate of the window
+     * @param yMin the minimum y coordinate of the window
+     * @param yMax the maximum y coordinate of the window
+     * @param invertedSegments a boolean that indicates if the segments are inverted or not (true when we treat vertical segments)
+     * @return the ArrayList of the reported segments
+     */
     public ArrayList<Segment> query(double xMin, double xMax, double yMin, double yMax, boolean invertedSegments) {
-        reportedSegments = new ArrayList<Segment>();
-        reportedSubTrees = new ArrayList<PrioritySearchTree>();
+        ArrayList<Segment> reportedSegments = new ArrayList<Segment>();
+        ArrayList<PrioritySearchTree> reportedSubTrees = new ArrayList<PrioritySearchTree>();
 
         //apply_window(xMin, yMin, xMax, yMax, reportedSegments);
         query_pst(xMin, xMax, yMin, yMax, reportedSegments, reportedSubTrees,false, false);
@@ -160,6 +198,23 @@ public class PrioritySearchTree {
         }
         return reportedSegments;
     }
+
+    /**
+     * This method query a PST in order to find the segments that are inside the window. It's an implementation of the reference pseudocode.
+     * First it checks that the current node is not null. Then it checks if the current node is inside the window.
+     * If it is, it checks if the current node is not already reported. If it's not, it adds the current node to the reportedSegments ArrayList.
+     * Then it checks if we already found the split node. If we did, it means that we need to explore both subtrees.
+     * If we didn't, it means that we need to explore only one subtree, the one where our y bounds are.
+     * The split case conditions allow us to prune some of the subtrees if they are not inside the window.
+     * @param xMin the minimum x coordinate of the window
+     * @param xMax the maximum x coordinate of the window
+     * @param yMin the minimum y coordinate of the window
+     * @param yMax the maximum y coordinate of the window
+     * @param reportedSegments the ArrayList of the reported segments
+     * @param reportedSubTrees the ArrayList of the reported subtrees that will be explored by the report_in_subtree() method
+     * @param split a boolean that indicates if we already found the split node
+     * @param maxSearching a boolean that indicates if we are searching for the maximum y coordinate of the window (true) i.e we are in the right subtree of the split node.
+     */
     private void query_pst(double xMin, double xMax, double yMin, double yMax, ArrayList<Segment> reportedSegments, ArrayList<PrioritySearchTree> reportedSubTrees, boolean split, boolean maxSearching){
         if(data!=null){
             double x = data.point.get_coord1();
@@ -242,6 +297,20 @@ public class PrioritySearchTree {
             }
         }
     }
+
+    /**
+     * This method explore the subtree of a given node in order to find the segments that are inside the window.
+     * This is an implementation of the report_in_subtree() method of the reference.
+     * Those subtrees are reported by the query_pst() method if and only if they are within the y bounds of the window.
+     * So that we only need to control that the x coordinate of the current node is within the x bounds of the window.
+     * If it is, we check if the current node is inside the window. If it is, we add it to the reportedSegments ArrayList.
+     * If the current node is not a leaf and the x coordinate is smaller than the xMax, we explore its subtrees.
+     * @param xMin the minimum x coordinate of the window
+     * @param xMax the maximum x coordinate of the window
+     * @param yMin the minimum y coordinate of the window
+     * @param yMax the maximum y coordinate of the window
+     * @param reportedSegments the ArrayList of the reported segments
+     */
     private void report_in_subtree(double xMin, double xMax, double yMin, double yMax, ArrayList<Segment> reportedSegments){
         if (data != null){
             double x = data.point.get_coord1();
@@ -264,6 +333,19 @@ public class PrioritySearchTree {
         }
     }
 
+    /**
+     * A simple method to determine if a segment intersect the left bound of the window.
+     * As we only consider the first endpoint of the endpoint which is the left extremity of the segment,
+     * we only need to check if we intersect the left bound of the window because the other endpoint is always to the right.
+     * If we do not intersect the bound it means that the segment is not in the window.
+     * As we flip vertical segments coordinates we don't need to adapt the algorithm for vertical segments.
+     * They are considered as horizontal segments.
+     * @param segment the segment we want to check
+     * @param xMin the minimum x coordinate of the window
+     * @param yMin the minimum y coordinate of the window
+     * @param yMax the maximum y coordinate of the window
+     * @return true if the segment intersect the window, false otherwise
+     */
     private boolean segmentIntersectWindow(Segment segment, double xMin, double yMin, double yMax){
         double x1 = segment.get_startComp().get_coord1();
         double y = segment.get_startComp().get_coord2();
